e4fba1b614dcacce6b954c88607b90a5a7d1d61c
two-pack <twopackas@gmail.com>
2017-10-02 23:07:18 +0900

    Add tests for GroupPatch.

---
+++ test/unit/group_test.rb
@@ -0,0 +1,17 @@
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
+
+module RedmineAutoAssignGroup
+  class GroupTest < ActiveSupport::TestCase
+    fixtures :users
+
+    ActiveRecord::FixtureSet.create_fixtures(
+        File.dirname(__FILE__) + '/../fixtures/', [:assign_rules])
+
+    def test_destroy_with_rules
+      Group.find(11).destroy
+
+      assert_equal 0, AssignRule.where(:group_id => 11).count
+    end
+
+  end
+end
