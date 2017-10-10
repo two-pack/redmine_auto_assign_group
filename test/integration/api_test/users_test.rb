24779acaf7b11cd941298508f54df9211de3797b
two-pack <twopackas@gmail.com>
2017-10-10 22:14:58 +0900

    Adds tests for REST API of user adding.

---
+++ test/integration/api_test/users_test.rb
@@ -0,0 +1,8 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../test_helper')
+
+if ((Redmine::VERSION::MAJOR == 3) && (Redmine::VERSION::MINOR >= 4)) or
+    (Redmine::VERSION::MAJOR >= 4) then
+  require File.expand_path(File.dirname(__FILE__) + '/users_test_latest')
+else
+  require File.expand_path(File.dirname(__FILE__) + '/users_test_33x')
+end
---
+++ test/integration/api_test/users_test_latest.rb
@@ -0,0 +1,80 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../test_helper')
+
+module RedmineAutoAssignGroup
+  class Redmine::ApiTest::UsersTest < Redmine::ApiTest::Base
+    fixtures :users, :email_addresses, :members, :member_roles, :roles, :projects,
+             :groups_users
+
+    ActiveRecord::FixtureSet.create_fixtures(
+        File.dirname(__FILE__) + '/../../fixtures/', [:assign_rules])
+
+    test "POST /users.xml with valid parameters should automatically assign group for new user" do
+      assert_difference('User.count') do
+        post '/users.xml',
+          :params => {
+            :user => {
+              :login => 'foo', :firstname => 'Firstname', :lastname => 'Lastname',
+              :mail => 'foo@abc.example.com', :password => 'secret123',
+              :mail_notification => 'only_assigned'
+            }
+          },
+          :headers => credentials('admin')
+      end
+
+      user = User.order('id DESC').first
+      assert_equal 11, user.groups.first.id
+    end
+
+    test "POST /users.json with valid parameters should automatically assign group for new user" do
+      assert_difference('User.count') do
+        post '/users.json',
+          :params => {
+            :user => {
+              :login => 'foo', :firstname => 'Firstname', :lastname => 'Lastname',
+              :mail => 'foo@abc.example.com', :password => 'secret123',
+              :mail_notification => 'only_assigned'
+            }
+          },
+          :headers => credentials('admin')
+      end
+
+      user = User.order('id DESC').first
+      assert_equal 11, user.groups.first.id
+    end
+
+    test "POST /users.xml with with invalid parameters should return errors" do
+      assert_no_difference('User.count') do
+        post '/users.xml',
+          :params => {
+            :user =>{
+              :login => 'foo', :lastname => 'Lastname', :mail => 'foo'
+            }
+          },
+          :headers => credentials('admin')
+      end
+
+      assert_response :unprocessable_entity
+      assert_equal 'application/xml', @response.content_type
+      assert_select 'errors error', :text => "First name cannot be blank"
+    end
+
+    test "POST /users.json with with invalid parameters should return errors" do
+      assert_no_difference('User.count') do
+        post '/users.json',
+          :params => {
+            :user => {
+              :login => 'foo', :lastname => 'Lastname', :mail => 'foo'
+            }
+          },
+          :headers => credentials('admin')
+      end
+
+      assert_response :unprocessable_entity
+      assert_equal 'application/json', @response.content_type
+      json = ActiveSupport::JSON.decode(response.body)
+      assert_kind_of Hash, json
+      assert json.has_key?('errors')
+      assert_kind_of Array, json['errors']
+    end
+  end
+end
